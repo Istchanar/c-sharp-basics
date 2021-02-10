5fb0a72d48d61bedab13f1dd4d32fe1beea28c50
Istchanar <istchanar@gmail.com>
2021-02-11 01:05:20 +0700

    fix_lesson6

--- lesson6/lesson6/Program.cs
+++ lesson6/lesson6/Program.cs
@@ -9,53 +9,145 @@ namespace Calculator
         {
             Console.SetWindowSize(130, 25);
 
-            Process[] processes = Process.GetProcesses();
+            Console.ForegroundColor = ConsoleColor.Green;
 
-            Console.WriteLine(string.Format("|{0,-20}|{1,-20}|{2,-20}|{3,-60}|", "processTime", "processId", "processMemory", "processName"));
+            Console.WriteLine(string.Format("|{0,20}|{1,20}|{2,20}|{3,50}|", "PROCESSTIME", "PROCESSID", "PROCESSMEMORY", "PROCESSNAME"));
 
-            for (int i = 0; i < processes.Length; i++)
-            {
+            ProcessList();
 
-                try
-                {
-                    string processName = $"{processes[i].ProcessName}";
-                    string processId = $"{processes[i].Id}";
-                    string processMemory = $"{processes[i].VirtualMemorySize64}";
-                    string processTime = $"{processes[i].StartTime}"; // Для некоторых процессов недоступно время, надо подумать как обработать;
-                    Console.WriteLine(string.Format("|{0,-20}|{1,-20}|{2,-20}|{3,-60}|", processTime, processId, processMemory, processName));
-                    //Console.WriteLine($"{processName} {processId} {processMemory}");
-                }
-                catch
-                {
-                    // Очень много времени занимает обработка в трай-кетч;
-                    // Console.WriteLine("Ошибка");
-                }
-            }
+            Console.ResetColor();
 
-            Console.WriteLine($"Общее кол-во процессов в системе {processes.Length}");
+            Notification($"Введите номер или имя процесса:");
 
-            Console.WriteLine($"Введите номер или имя процесса: ");
+            string processName = Console.ReadLine(); // Получаю имя процесса;
 
-            string proc = Console.ReadLine(); // Получаю имя процесса;
+            bool parseId = int.TryParse(processName, out int id); // пытаюсь парсить - распарсилось значит айди, нет - значит имя;
 
-            bool x = int.TryParse(proc, out int z); // пытаюсь парсить - распарсилось значит айди, нет - значит имя;
-
-            if (x == true)
-            {
-                Process proccToKill = Process.GetProcessById(z); // убиваю по айди
-                proccToKill.Kill();
-            }
-            else
-            {
-                foreach (var process in Process.GetProcessesByName(proc)) // ищу имя в списке всех процессов и убиваю по имени;
-                {
-                    process.Kill();
-                }
-            }
-
-            Console.WriteLine("Процесс убит");
+            GetByNameOrId(parseId, id, processName);
 
             Console.ReadKey();
         }
+
+        //Метод выполняет два метода, принимая три переменных
+
+        static void GetByNameOrId(bool parseId, int id, string processName)
+        {
+            if (parseId == true)
+            {
+                GetById(id);
+            }
+            else
+            {
+                GetByName(processName);
+            }
+
+        }
+
+        //Метод по name завершает процесс:
+
+        static void GetByName(string processName)
+        {
+            try
+            {
+                foreach (Process process in Process.GetProcessesByName(processName))
+                {
+
+                    process.Kill();
+
+                    Notification($"Процесс name={processName} завершён.");
+                }
+            }
+            catch
+            {
+                Notification($"Процесса name={processName} не существует.");
+            }
+
+
+        }
+
+        // Метод по id завершает процесс:
+
+        static void GetById(int id)
+        {
+            try
+            {
+                Process process = Process.GetProcessById(id);
+
+                process.Kill();
+
+                Notification($"Процесс pid={id} завершён.");
+            }
+            catch
+            {
+                Notification($"Процесса pid={id} не существует.");
+            }
+
+        }
+
+        // Метод сообщений:
+
+        static void Notification(string information)
+        {
+            Console.WriteLine();
+
+            Console.WriteLine(information);
+        }
+
+        static void ProcessList()
+        {
+
+            Process[] processes = Process.GetProcesses();
+
+            uint error = 0;
+
+            foreach (Process targetProcess in processes)
+            {
+                try
+                {
+                    Console.Write(string.Format("|{0,20}|", DateTime.Now.Subtract(targetProcess.StartTime).TotalMinutes.ToString("F2")));
+                }
+                catch (Exception ex)
+                {
+                    Console.Write(string.Format("|{0,20}|", ex.Message));
+
+                    error++;
+
+                }
+
+
+                try
+                {
+                    Console.Write(string.Format("{0,20}|", targetProcess.Id.ToString()));
+                }
+                catch (Exception ex)
+                {
+                    Console.Write(string.Format("{0,20}|", ex.Message));
+
+                    error++;
+                }
+
+                try
+                {
+                    // Из long превращаем int, и делим на 1000000 получаем память. 
+
+                    int mBite = (int)targetProcess.VirtualMemorySize64 / 1000000;
+
+                    Console.Write(string.Format("{0,20}|", mBite.ToString(), "mb"));
+                }
+                catch (Exception ex)
+                {
+                    Console.Write("{0, 20}|", ex.Message);
+
+                    error++;
+                }
+
+                Console.Write("{0,50}|", targetProcess.ProcessName);
+
+                Console.WriteLine();
+            }
+
+            Notification($"Процессов: {processes.Length}, ошибок доступа: {error}");
+        }
     }
 }
+
